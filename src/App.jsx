b5a0159239b8e9cdd96aed171141a1c674a8539f
Pedro Alves <pedrost00700@gmail.com>
2023-12-06 16:07:28 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import Perfil from "./components/Perfil";
+import Formulario from "./components/formulario";
+import ReposList from "./components/ReposList";
+
+
+function App() {
+  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState('');
+
+  return (
+    <>
+      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+
+      {nomeUsuario.length > 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario} />
+          <ReposList nomeUsuario={nomeUsuario} />
+        </>
+      )}
+
+      {/* {formularioEstaVisivel && (
+      <Formulario />
+    )}
+
+    <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button> */}
+    </>
+  )
+}
+
+export default App
