9df015f6ac2a6252b5a669a2d5de0e30050f971a
Pedro Alves <pedrost00700@gmail.com>
2023-12-06 16:15:44 -0300

    mensagem de erro caso não encontre o usuário

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,31 +1,42 @@
 import { useEffect, useState } from "react";
-
 import styles from './ReposList.module.css';
 
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [mensagemErro, setMensagemErro] = useState(""); 
 
     useEffect(() => {
         setEstaCarregando(true);
+        setMensagemErro(""); 
+
         fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-            .then(res => res.json())
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`Erro ao buscar repositórios: ${res.status}`);
+                }
+                return res.json();
+            })
             .then(resJson => {
                 setTimeout(() => {
                     setEstaCarregando(false);
                     setRepos(resJson);
                 }, 3000);
-
             })
-    }, [nomeUsuario])
+            .catch(error => {
+                setEstaCarregando(false);
+                setMensagemErro(`Usuário "${nomeUsuario}" não encontrado. Verifique o nome e tente novamente.`);
+            });
+    }, [nomeUsuario]);
 
     return (
         <div className="container">
             {estaCarregando ? (
                 <h1>Carregando...</h1>
+            ) : mensagemErro ? (
+                <h1>{mensagemErro}</h1>
             ) : (
                 <ul className={styles.list}>
-                    {/* {repos.map(repositorio => ( */}
                     {repos.map(({ id, name, language, html_url }) => (
                         <li className={styles.listItem} key={id}>
                             <div className={styles.itemName}>
@@ -42,7 +53,7 @@ const ReposList = ({ nomeUsuario }) => {
                 </ul>
             )}
         </div>
-    )
-}
+    );
+};
 
 export default ReposList;
